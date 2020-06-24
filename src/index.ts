59bbde29961a4db877aa1472df77bb5ab0606195
gaojia <gaojia@goodrock.com.cn>
2020-06-24 17:06:19 +0800

    成功连接数据库

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import {createConnection} from "typeorm";
 
 createConnection().then(async connection => {
     console.log(connection)
